f455f0898c3940fcdb28f9939e333370fda762be
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-04-30 16:45:39 +0200

    feat: added Device.select()

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,50 @@
 import { Platform } from 'react-native';
 import RNDeviceInfo from 'react-native-device-info';
-import { DeviceOS, DeviceType } from './types';
+import { DeviceOS, DeviceSelectType, DeviceType } from './types';
 
-export * from './types';
+type DeviceSelect = <T = undefined>(specifics: {
+  [type in DeviceSelectType]?: T;
+}) => T | undefined;
+
+const deviceSelect: DeviceSelect = <T>(specifics: {
+  [type in DeviceSelectType]?: T;
+}) => {
+  let styles = Platform.select(specifics);
+
+  if (specifics.androidTV && Device.isAndroidTV) {
+    styles = { ...styles, ...specifics.androidTV };
+  }
+
+  if (specifics.appleTV && Device.isAppleTV) {
+    styles = { ...styles, ...specifics.appleTV };
+  }
+
+  if (specifics[DeviceType.TV] && Device.isTV) {
+    styles = { ...styles, ...specifics[DeviceType.TV] };
+  }
+
+  if (specifics.mobile && Device.isMobile) {
+    styles = { ...styles, ...specifics.mobile };
+  }
+
+  if (specifics[DeviceType.Phone] && Device.isPhone) {
+    styles = { ...styles, ...specifics[DeviceType.Phone] };
+  }
+
+  if (specifics[DeviceType.Tablet] && Device.isTablet) {
+    styles = { ...styles, ...specifics[DeviceType.Tablet] };
+  }
+
+  if (specifics[DeviceOS.Apple] && Device.isApple) {
+    styles = { ...styles, ...specifics[DeviceOS.Apple] };
+  }
+
+  if (specifics.default) {
+    styles = { ...styles, ...specifics.default };
+  }
+
+  return styles;
+};
 
 export const Device = {
   get isTablet() {
@@ -88,4 +130,8 @@ export const Device = {
 
     return DeviceType.Unknown;
   },
+
+  select: deviceSelect,
 };
+
+export * from './types';
